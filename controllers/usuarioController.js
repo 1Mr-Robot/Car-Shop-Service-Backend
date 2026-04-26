ae05f7cb75211d42242bc4062395803b3a56be71
Uziel Flores <uzielo8017518@gmail.com>
2026-04-26 16:52:02 -0600

    Funcionalidad al nombre de usuario en pantalla CreateOrderScreen

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -20,4 +20,26 @@ const getMecanicos = async (req, res) => {
     }
 };
 
-module.exports = { getMecanicos };
+const getCurrentUser = async (req, res) => {
+    try {
+        const query = `
+            SELECT 
+                nombre, 
+                apellido_paterno
+            FROM usuario
+            WHERE id = $1
+        `;
+        const result = await pool.query(query, [req.user.id]);
+        
+        if (result.rows.length === 0) {
+            return res.status(404).json({ error: "Usuario no encontrado" });
+        }
+
+        res.status(200).json({ data: result.rows[0] });
+    } catch (error) {
+        console.error("Error en getCurrentUser:", error);
+        res.status(500).json({ error: "Error al obtener usuario." });
+    }
+};
+
+module.exports = { getMecanicos, getCurrentUser };
--- routes/usuarioRoutes.js
+++ routes/usuarioRoutes.js
@@ -2,4 +2,5 @@ const express = require("express");
 const router = express.Router();
 const usuarioController = require("../controllers/usuarioController");
 router.get("/mecanicos", usuarioController.getMecanicos);
+router.get("/yo", usuarioController.getCurrentUser);
 module.exports = router;
